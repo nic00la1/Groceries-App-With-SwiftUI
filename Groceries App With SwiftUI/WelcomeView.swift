7593f505864294ea43c3b4785fe22850152225fc
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-09 23:42:04 +0100

    Design button ✨

--- Groceries App With SwiftUI/WelcomeView.swift
+++ Groceries App With SwiftUI/WelcomeView.swift
@@ -14,6 +14,24 @@ struct WelcomeView: View {
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
+            VStack{
+                Spacer()
+                Button {
+                    // Action
+                    
+                } label: {
+                    
+                    Text("Get Started")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 18, weight: .semibold, design: .rounded))
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
+                .background(.green)
+                .cornerRadius(12)
+                .padding(.horizontal, 30)
+                .padding(.bottom, 90)
+            }
         }
     }
 }
