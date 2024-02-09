a4133f2b6615de8da412594e98513cdbe3b6e7a9
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-09 23:56:40 +0100

    simple Navigation works properly

--- Groceries App With SwiftUI/WelcomeView.swift
+++ Groceries App With SwiftUI/WelcomeView.swift
@@ -8,44 +8,54 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var presentCategories = false
+    
     var body: some View {
-        ZStack {
-            Image(uiImage: Images.Welcome.background)
-                .resizable()
-                .scaledToFill()
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Spacer()
-                Image(uiImage: Images.Welcome.logo)
+        
+        NavigationStack {
+            ZStack {
+                Image(uiImage: Images.Welcome.background)
                     .resizable()
-                    .scaledToFit()
-                    .frame(width: 50)
-                
-                Text("Welcome to our store")
-                    .font(.system(size: 48, weight: .semibold, design: .rounded))
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.white)
-                    .padding(.bottom, 8)
-                
-                Text("Get Your groceries in as fast as one hour")
-                    .font(.system(size: 18, weight: .regular, design: .rounded))
-                    .foregroundStyle(.gray)
-                    .padding(.bottom, 40)
-                Button {
-                    // Action
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    Image(uiImage: Images.Welcome.logo)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50)
                     
-                } label: {
-                    
-                    Text("Get Started")
+                    Text("Welcome to our store")
+                        .font(.system(size: 48, weight: .semibold, design: .rounded))
+                        .multilineTextAlignment(.center)
                         .foregroundStyle(.white)
-                        .font(.system(size: 18, weight: .semibold, design: .rounded))
+                        .padding(.bottom, 8)
+                    
+                    Text("Get Your groceries in as fast as one hour")
+                        .font(.system(size: 18, weight: .regular, design: .rounded))
+                        .foregroundStyle(.gray)
+                        .padding(.bottom, 40)
+                    
+                    Button {
+                        // Action
+                        presentCategories.toggle()
+                        
+                    } label: {
+                        
+                        Text("Get Started")
+                            .foregroundStyle(.white)
+                            .font(.system(size: 18, weight: .semibold, design: .rounded))
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16)
+                    .background(.green)
+                    .cornerRadius(12)
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 90)
                 }
-                .frame(maxWidth: .infinity)
-                .padding(.vertical, 16)
-                .background(.green)
-                .cornerRadius(12)
-                .padding(.horizontal, 30)
-                .padding(.bottom, 90)
+            }
+            .navigationDestination(isPresented: $presentCategories) {
+                Text("Categories Screen")
             }
         }
     }
